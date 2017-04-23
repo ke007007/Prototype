a04d636a179e3372bf66186cd4ff7b3449e032bb
Keita Takahashi <Keita Takahashi>
2017-04-23 17:13:45 +0900

    20170423_17

---
+++ Assets/Scripts/RightController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightController : MonoBehaviour
+{
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Ray ray = new Ray (transform.position, transform.forward);
+		if (Physics.Raycast (ray, out hit)) {
+			print (hit.collider.name); 
+		}
+
+	}
+}
